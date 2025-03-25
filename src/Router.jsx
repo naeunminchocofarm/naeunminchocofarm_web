30f8f24874c5be93365fe1b838ae5eb92c95217f
eyna0414 <pey0414@gmail.com>
2025-03-25 17:33:50 +0900

    하루습도그래프 기능구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
+import ExamChart from "./humidity/pages/ExamChart";
 
 export default function Router() {
   return (
     <>
       <Routes>
+        <Route path="/examples/chart" element={<ExamChart />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
