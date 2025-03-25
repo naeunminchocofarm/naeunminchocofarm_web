4204bbc83bbf75e8ef035da5e9bc54dbf08614fd
Lin-Bee <lin_bee@naver.com>
2025-03-25 15:35:55 +0900

    온도페이지 작업후 출력 여부 확인 후 업로드

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import Router from "./Router";
 import './App.css'
+import Temperature from './temperature/temperature';
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <Temperature/>
+      <Router/>
     </BrowserRouter>
   )
 }
