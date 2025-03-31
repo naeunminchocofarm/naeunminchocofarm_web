ab602deba603cd7922d37bc5e71da0652c2be4eb
eyna0414 <pey0414@gmail.com>
2025-03-31 14:25:38 +0900

    일조량 차트 작업 중

--- src/sunshine/components/Sunshine.jsx
+++ src/sunshine/components/Sunshine.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import Card from '../../common_components/Card';
 import CurrentSunshine from './CurrentSunshine';
 import SunshineChart from './SunshineChart';
 import Awning from './Awning';
+import sunshineApi from '../../api/sunshineApi'; 
 
 const Sunshine = () => {
   const [sunshines, setSunshines] = useState([]);
   // 현재 일조량 데이터
   const [currentSunshine, setCurrentSunshine] = useState(0);
+
+   // API에서 데이터를 가져오는 useEffect
+  useEffect(() => {
+    const fetchSunshineData = async () => {
+      try {
+        const response = await sunshineApi.getAll();
+        console.log(response.data);
+        setSunshines(response.data);
+
+        // 현재 일조량 업데이트
+        if (response.data.length > 0) {
+          setCurrentSunshine(response.data[response.data.length - 1].sunshineValue);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSunshineData();
+    const intervalId = setInterval(fetchSunshineData, 600000); // 10분마다 데이터 갱신
+    return () => clearInterval(intervalId);
+  }, []);
   
   return (
     <>
@@ -25,7 +48,7 @@ const Sunshine = () => {
 
       {/* 하루 일조량 그래프 */}
       <Card>
-        <SunshineChart sunshines={sunshines} />
+        <SunshineChart sunshines={sunshines}/>
       </Card>
     </>
   );
--- src/sunshine/components/SunshineChart.jsx
+++ src/sunshine/components/SunshineChart.jsx
@@ -12,8 +12,18 @@ Chart.register(...registerables);
 ChartJs.register(LineElement, CategoryScale, LinearScale, PointElement);
 
 const SunshineChart = ({ sunshines, responsive = true, width = 400, height = 300 }) => {
+  // 1시간 단위 데이터만 필터링 (각 시간대 첫 번째 데이터만 유지)
+  const filteredSunshines = sunshines.reduce((acc, current) => {
+    const hour = new Date(current.measuredAt).getHours();
+    if (!acc.some((item) => new Date(item.measuredAt).getHours() === hour)) {
+      acc.push(current);
+    }
+    return acc;
+  }, []);
+
   // X축 시간 레이블
-  const labels = Array.from({ length: 24 }, (_, i) => `${i + 1}시`);
+  const labels = filteredSunshines.map((s) => new Date(s.measuredAt).getHours() + "시");
+  //Array.from({ length: 24 }, (_, i) => `${i + 1}시`);
 
   // 일조량 데이터
   const sunshineData = {
@@ -21,7 +31,7 @@ const SunshineChart = ({ sunshines, responsive = true, width = 400, height = 300
     datasets: [
       {
         label: "일조량(kWh)",
-        data: sunshines.map((x) => x.value),
+        data: sunshines.map((x) => x.sunshineValue),
         borderColor: "black",
         backgroundColor: "black",
         // 선의 곡률
