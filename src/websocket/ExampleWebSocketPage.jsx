0850f48a36da2911a0aae1bfe6a917e9d4bfd012
eyna0414 <pey0414@gmail.com>
2025-03-31 15:03:19 +0900

    웹소켓 테스트를 위한 경로 변경

--- src/websocket/ExampleWebSocketPage.jsx
+++ src/websocket/ExampleWebSocketPage.jsx
@@ -3,22 +3,25 @@ import NcfSubscriber from "./ncf_subscriber";
 import { webSocketPaths, subscribePaths } from "./wobsocket_paths";
 
 export default function ExampleWebSocketPage() {
-  const [message, setMessage] = useState('');
-  const [received, setReceived] = useState('');
+  const [message, setMessage] = useState("");
+  const [received, setReceived] = useState("");
   const socketClient = useRef(undefined);
 
   useEffect(init, []);
 
   function init() {
-    const ncfSubscriber = new NcfSubscriber(webSocketPaths.local, subscribePaths.testSubject);
+    const ncfSubscriber = new NcfSubscriber(
+      webSocketPaths.dev,
+      subscribePaths.testSubject
+    );
 
-    ncfSubscriber.onOpen = function(e) {
+    ncfSubscriber.onOpen = function (e) {
       ncfSubscriber.subscribe();
-    }
+    };
 
-    ncfSubscriber.onMessage = function(frame) {
+    ncfSubscriber.onMessage = function (frame) {
       setReceived(frame.body);
-    }
+    };
 
     socketClient.current = ncfSubscriber;
 
@@ -27,31 +30,42 @@ export default function ExampleWebSocketPage() {
         socketClient.current.close();
         socketClient.current = undefined;
       }
-    }
+    };
   }
 
   function sendMessage() {
-    if (socketClient.current && socketClient.current.getReadyState() === WebSocket.OPEN) {
+    if (
+      socketClient.current &&
+      socketClient.current.getReadyState() === WebSocket.OPEN
+    ) {
       socketClient.current.send(message);
     }
-    
-    setMessage('');
+
+    setMessage("");
   }
 
   const containerCss = "p-5";
   const titleCss = "text-4xl font-bold mb-10";
   const inputCss = "border border-gray-600 rounded-lg outline-purple-600 mx-3";
-  const btnCss = "px-5 rounded-md hover:cursor-pointer hover:bg-purple-600 hover:text-white border border-purple-600"
+  const btnCss =
+    "px-5 rounded-md hover:cursor-pointer hover:bg-purple-600 hover:text-white border border-purple-600";
 
   return (
     <>
       <div className={containerCss}>
         <h1 className={titleCss}>WebSocket Example Page</h1>
         <label>
-          text: 
-          <input type="text" className={inputCss} value={message} onChange={e => setMessage(e.target.value)}/>
+          text:
+          <input
+            type="text"
+            className={inputCss}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </label>
-        <button type="button" className={btnCss} onClick={sendMessage}>send</button>
+        <button type="button" className={btnCss} onClick={sendMessage}>
+          send
+        </button>
         <div>server: {received}</div>
       </div>
     </>
