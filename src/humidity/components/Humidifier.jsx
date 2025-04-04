0ef55719ff324b4933f1cbac41d0b8b0d6a46c2d
eyna0414 <pey0414@gmail.com>
2025-04-04 15:17:17 +0900

    습도 대시보드 작업 중

--- src/humidity/components/Humidifier.jsx
+++ src/humidity/components/Humidifier.jsx
@@ -1,56 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import CurrentHumidity from "./CurrentHumidity";
+import ToggleButton from '../../common_components/ToggleButton';
 
-const Humidifier = () => {
+const Humidifier = ({currentHumidity}) => {
+  // 가습기 작동 여부
   const [autoHumidifier, setAutoHumidifier] = useState(false);
-  
+  const [manualHumiditier, setManualHumiditier] = useState(false);
+
+
   useEffect(() => {
     //습도에 따른 가습기 상태 업데이트
     //ON : 현재습도 60% 미만
     //OFF: 현재습도 80% 초과
-    if(CurrentHumidity < 60){
+    if(currentHumidity < 60){
       setAutoHumidifier(true);
     } else if (CurrentHumidity > 80){
       setAutoHumidifier(false);
     }
-  }, [CurrentHumidity]);
+  }, [currentHumidity]);
 
   return (
-    <>
-      <div
-        onClick={() => setAutoHumidifier(prev => !prev)}
-        style={{
-          position: 'relative',
-          display: 'inline-block',
-          width: '60px',
-          height: '30px',
-          backgroundColor: autoHumidifier ? '#4CAF50' : '#ccc',
-          borderRadius: '15px',
-          cursor: 'pointer',
-          transition: 'background-color 0.3s ease',
-        }}
-      >
-        {/* 토글 내부 원 */}
-        <div
-          style={{
-            position: 'absolute',
-            top: '3px',
-            left: autoHumidifier ? '33px' : '3px',
-            width: '24px',
-            height: '24px',
-            backgroundColor: 'white',
-            borderRadius: '50%',
-            transition: 'left 0.3s ease',
-            boxShadow: '0 2px 5px rgba(0,0,0,0.3)',
-          }}
-        />
-      </div>
+    <div>
+      <ToggleButton 
+        value={manualHumiditier} 
+        // onChange={}
+      />
 
-      {/* 자동 급수 시스템 상태 표시 */}
-      <div style={{fontSize: '2rem', fontWeight: 'bold'}}>
-        자동 급수 시스템: {autoHumidifier ? '작동 중' : '중지'}
-      </div>
-    </>
+    </div>
   )
 }
 
