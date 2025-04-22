c9d247b2233351a2d954c9d8d080eafaa82e62ce
koeyismyusername <koey2182@gmail.com>
2025-04-22 17:10:20 +0900

    사용되지 않는 코드 주석처리

--- src/members/components/ProtectedAdminRoute.jsx
+++ src/members/components/ProtectedAdminRoute.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+// import { isAdmin, isAdminV2 } from "../apis/authCheck";
 import { isAdmin } from "../apis/authCheck";
 import FullPageSpinner from "../../pages/FullPageSpinner";
+import { useLoginInfo } from "../../redux/store";
 
 const ProtectedAdminRoute = ({ children }) => {
-  const token = useSelector((state) => state.auth.token);
+  // const token = useSelector((state) => state.auth.token);
+  const loginInfo = useLoginInfo();
   const [isAccessible, setIsAccessible] = useState(null);
 
   useEffect(() => {
-    if (!isAdmin(token)) {
+    if (!isAdmin(loginInfo)) {
       alert("접근할 수 없습니다.\n첫 화면으로 이동합니다.");
       setIsAccessible(false);
     } else {
       setIsAccessible(true);
     }
-  }, [token]);
+  }, [loginInfo]);
 
   if (isAccessible === null) return <FullPageSpinner />;
   if (!isAccessible) return <Navigate to="/web/home" replace />;
--- src/members/components/ProtectedRoute.jsx
+++ src/members/components/ProtectedRoute.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { isFammer } from "../apis/authCheck";
+import { useLoginInfo } from "../../redux/store";
 
 const ProtectedRoute = ({ children }) => { 
-  const token = useSelector((state) => state.auth.token);
+  // const token = useSelector((state) => state.auth.token);
+  const loginInfo = useLoginInfo();
   const [isAccessible, setIsAccessible] = useState(null);
 
   useEffect(() => {
-    if (!isFammer(token)) {
+    if (!isFammer(loginInfo)) {
       alert("로그인이 필요합니다.\n첫 화면으로 이동합니다.");
       setIsAccessible(false);
     } else {
       setIsAccessible(true);
     }
-  }, [token]);
+  }, [loginInfo]);
 
   if (isAccessible === null) return null;
   return isAccessible ? children : <Navigate to={"/web/home"} />;
